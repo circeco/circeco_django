41a0286b3c3b61dc52b29d36b42e1e48a3476376
Piero <grilli.piero@circeco.org>
2020-05-25 12:33:34 +0000

    dj_database_url installed for deploying to heroku

--- circeco_django/settings.py
+++ circeco_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,14 @@ WSGI_APPLICATION = 'circeco_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+#    }
+
+DATABASE = {'default': dj_database_url.parse('postgres://oqgxvetmemhufz:4003d612d9f0988cf8283ef6b846ba19f17b4ae5e8d6a75b45390c27fbcaa889@ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/ddbfk59hpjrsem')}
 
 
 # Password validation
