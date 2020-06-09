e44f20557f9be2160c4d373538a4d18e72e33bef
Piero <grilli.piero@circeco.org>
2020-06-09 06:52:05 +0000

    aws bucket name change

--- circeco_django/settings.py
+++ circeco_django/settings.py
@@ -157,7 +157,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 
-AWS_STORAGE_BUCKET_NAME = 'ecommerce'
+AWS_STORAGE_BUCKET_NAME = 'circeco-ecommerce'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
