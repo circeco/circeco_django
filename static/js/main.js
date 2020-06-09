51f5781fba3da5b8f954b879cbd417597e5de402
Piero <grilli.piero@circeco.org>
2020-06-09 16:30:11 +0000

    dont ignore static

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+/*---------Materialize JS code--*/
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    $('select').material_select();
+    $('.tooltipped').tooltip();
+    $('.modal').modal();
+});
