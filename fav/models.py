0baa3bd9c15c9411024f1819ba22e5fccb8d75e6
Piero <grilli.piero@circeco.org>
2020-06-14 20:50:32 +0000

    implement fav delete with unique constraint

--- fav/models.py
+++ fav/models.py
@@ -3,8 +3,12 @@ from django.contrib.auth.models import User
 from shops.models import Shop
 
 
+
 class UserFavouriteShop(models.Model):
     user = models.ForeignKey(User, null=False)
     shop = models.ForeignKey(Shop, null=False)
-    # TODO UNIQUE/INDEX (user, shop)
+
+    class Meta:
+        unique_together = ('user', 'shop')
+        # constraints = [models.UniqueConstraint(fields=['user', 'shop'], name='user_shop')]
 
--- fav/views.py
+++ fav/views.py
@@ -6,7 +6,7 @@ from shops.models import Shop
 # Create your views here.
 def view_fav(request):
     """A View that renders the fav contents page"""
-    favs = UserFavouriteShop.objects.all().filter(user = request.user)
+    favs = UserFavouriteShop.objects.all().filter(user = request.user) # TODO "project" only shop 
     return render(request, "fav.html", {"fav_items": favs})
 
 
@@ -18,7 +18,7 @@ def add_to_fav(request, shopId):
 
     print(shopId, userId)
 
-    shop = Shop.objects.get(id=shopId)
+    shop = Shop.objects.get(id=shopId) # TODO handle exception
     newFav = UserFavouriteShop(user=userId, shop=shop)
     newFav.save()
 
@@ -32,13 +32,9 @@ def remove_fav(request, shopId):
     Adjust the quantity of the specified product to the specified
     amount
     """
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
-
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
     
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+    shop = Shop.objects.get(id=shopId)
+    afav = UserFavouriteShop.objects.get(user=request.user, shop=shop) # TODO handle exception
+    afav.delete() 
+
+    return redirect(reverse('view_fav'))
