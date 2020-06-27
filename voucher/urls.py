7d80a4516e9f233903ca0b520a23718f8ac5f31e
Piero <grilli.piero@circeco.org>
2020-06-27 08:45:58 +0000

    displaying QR and connecting to qrcode-monkey service

--- voucher/urls.py
+++ voucher/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import choose_voucher, create_voucher, verify_voucher
+from .views import choose_voucher, create_voucher, verify_voucher, display_QR
 
 urlpatterns = [
     url(r'^$', choose_voucher, name='choose_voucher'),
     url(r'^create/', create_voucher, name='create_voucher'),
+    url(r'^display/(?P<id>\d+)', display_QR, name='voucher'),
     url(r'^verify/(?P<id>\d+)', verify_voucher, name='verify_voucher'),
 ]
--- voucher/views.py
+++ voucher/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.urls.base import reverse
 from voucher.models import Voucher
 import stripe
+import http.client
 from django.contrib import messages
 from .forms import BuyVoucherForm
 from circeco_django import settings
+import json
+from django.http.response import HttpResponse
 
 stripe.api_key = settings.STRIPE_SECRET
 
@@ -46,20 +49,75 @@ def create_voucher(request):
         newVoucher = Voucher(user=user, amount=amount)
         newVoucher.save()
         print(newVoucher.id)
-
-        targetUrl = request.build_absolute_uri(reverse('verify_voucher', args=(newVoucher.id,)))
-        # TODO This should be the URL that goes into the QR
-
-        return render(request, 'view_voucher.html', {"voucher": newVoucher, "targetUrl": targetUrl})
+        
+        return display_QR(request, newVoucher.id)
     else:
         messages.error(request, "Unable to take payment")
 
     # If we get here there has been no payment!
-
     return render(request, 'choose_voucher.html', {"form": form, "publishable": settings.STRIPE_PUBLISHABLE})
 
+def display_QR(request, id):
+    voucher = Voucher.objects.get(id=id) # TODO check exception if not found
+    # TODO Check voucher belongs to user
+    targetUrl = request.build_absolute_uri(reverse('verify_voucher', args=(id,)))
+    image = makeQR(targetUrl)   
+    response = HttpResponse(image, content_type='image/png')
+    return response
 
 # login not required
 def verify_voucher(request, id):
     voucher = Voucher.objects.get(id=id)
     return render(request, 'verify_voucher.html', {"voucher": voucher})
+
+
+def makeQR(myData):
+    conn = http.client.HTTPSConnection("qrcode-monkey.p.rapidapi.com")
+
+    p = {
+        "data":myData,
+        "config":{
+            "body":"circle-zebra-vertical",
+            "eye":"frame13",
+            "eyeBall":"ball15",
+            "erf1":[],
+            "erf2":[],
+            "erf3":[],
+            "brf1":[],
+            "brf2":[],
+            "brf3":[],
+            "bodyColor":"#0277BD",
+            "bgColor":"#FFFFFF",
+            "eye1Color":"#075685",
+            "eye2Color":"#075685",
+            "eye3Color":"#075685",
+            "eyeBall1Color":"#0277BD",
+            "eyeBall2Color":"#0277BD",
+            "eyeBall3Color":"#0277BD",
+            "gradientColor1":"#075685",
+            "gradientColor2":"#0277BD",
+            "gradientType":"linear",
+            "gradientOnEyes":False,
+            "logo":"#facebook"
+            },
+        "size":600,
+        "download":False,
+        "file":"png"
+    }
+    
+    payload = json.dumps(p)
+
+    headers = {
+        'x-rapidapi-host': "qrcode-monkey.p.rapidapi.com",
+        'x-rapidapi-key': "02ee0a6910msh7ab1091612dfbacp185d5ejsnfcc7e4d94ae1",
+        'content-type': "application/json",
+        'accept': "application/json"
+        }
+
+    conn.request("POST", "/qr/custom", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data)
+    return data
