3c33e5f11af82889e9bd303481a7cc9ca1071cc0
Piero <grilli.piero@circeco.org>
2020-07-05 11:47:32 +0000

    implements the rendering of initial favourite state of shops

--- shops/views.py
+++ shops/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import Shop
+from fav.models import UserFavouriteShop
 
 
 @login_required()
 @ensure_csrf_cookie
 def all_shops(request):
     shops = Shop.objects.all()
-    return render(request, "shops.html", {"shops": shops})
+    favs = UserFavouriteShop.objects.all().filter(user = request.user) 
+    favShopIds = set()
+    for fav in favs: 
+        favShopIds.add(fav.shop.id) 
+    return render(request, "shops.html", {"shops": shops, "favs": favShopIds})
