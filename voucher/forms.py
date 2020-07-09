bf3c6880d17c469657a5d4acf3f84526815f8da5
Piero <grilli.piero@circeco.org>
2020-07-09 09:00:12 +0000

    styling of Vouchers and Buy Voucher page

--- voucher/forms.py
+++ voucher/forms.py
@@ -7,7 +7,7 @@ class BuyVoucherForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
     VOUCHER_AMOUNT = [(i, i) for i in [100,200,300]]
 
-    amount = forms.ChoiceField(label='Amount', choices=VOUCHER_AMOUNT, required=True)
+    amount = forms.ChoiceField(label='Amount in Swedish kr', choices=VOUCHER_AMOUNT, required=True)
 
     credit_card_number = forms.CharField(label='Credit card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
