c4327067b2a7eaf6db749d5ab9f4b77f92579f91
Piero <grilli.piero@circeco.org>
2020-07-12 19:25:05 +0200

    Fix premature message about successful login

--- accounts/views.py
+++ accounts/views.py
@@ -11,8 +11,9 @@ from shops.forms import SearchShopsForm
 
 def index(request):
     """Return the index.html file"""
-    form = SearchShopsForm() 
-    return render(request,  'index.html', { "form": form})
+    form = SearchShopsForm()
+    return render(request, 'index.html', {"form": form})
+
 
 @login_required
 def logout(request):
@@ -26,21 +27,20 @@ def login(request):
     """Return a login page"""
     if request.user.is_authenticated:
         return redirect(reverse('index'))
+
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
-
         if login_form.is_valid():
-            user = auth.authenticate(username=request.POST['username'],
-                                    password=request.POST['password'])
-            messages.success(request, "You have successfully logged in!")
-
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
             if user:
                 auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
                 return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
         login_form = UserLoginForm()
+
     return render(request, 'login.html', {'login_form': login_form})
 
 
@@ -67,9 +67,10 @@ def registration(request):
     return render(request, 'registration.html', {
         "registration_form": registration_form})
 
+
 def user_profile(request):
     """The user's profile page"""
     user = User.objects.get(email=request.user.email)
-    favs = UserFavouriteShop.objects.all().filter(user = request.user)
-    vouchers = Voucher.objects.filter(user = user)
+    favs = UserFavouriteShop.objects.all().filter(user=request.user)
+    vouchers = Voucher.objects.filter(user=user)
     return render(request, 'profile.html', {"profile": user, "fav_items": favs, "vouchers": vouchers})
